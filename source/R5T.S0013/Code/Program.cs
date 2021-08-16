8dffd6a4ee48bba6c792d849b7b68aabebab866f
David Coats <david@getsafetycone.com>
2021-08-16 11:17:20 -0700

    Add new .NET 5 functionality.

---
+++ source/R5T.S0013/Code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.S0013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
